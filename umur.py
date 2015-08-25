7244140094ed98e6855cf6e7d74dde437c95af63
alvian <alviandk@gmail.com>
2015-08-25 09:26:18 +0700

    first commit

---
+++ umur.py
@@ -0,0 +1,9 @@
+umur=input("Berapa umur anda: ")
+if umur<20:
+    print "masih bocah lo"
+    print "hahaha"
+elif umur==20:
+	print "oh umurnya 20"
+else:
+	print "sudah cukup dewasa"
+print "done"
