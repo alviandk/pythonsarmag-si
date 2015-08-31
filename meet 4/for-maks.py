ef8fb1cd396bf581360341c1e58940f9fe27c9e0
alvian <alviandk@gmail.com>
2015-08-31 11:37:26 +0700

    for dan fizzbuzz

---
+++ meet 4/for-maks.py
@@ -0,0 +1,9 @@
+batasan=input("angka maksimal pada deret: ")
+for loop in range (1,batasan+1):
+	print loop,
+print ''
+
+for loop in range (batasan,0,-1):
+	print loop,
+    
+print ''
---
+++ meet 4/for.py
@@ -0,0 +1,17 @@
+#print 1 2 3 4 5
+for loop in range (1,6):
+    print loop,
+#print "enter"
+print ""
+
+#print 0 2 4 6 8 (step 2) 
+for loop in range (0,10,2):
+    print loop,
+#print "enter"
+print ""
+
+# print 5 4 3 2 1 (step -1)
+for loop in range (5,0,-1):
+    print loop,
+#print "enter"
+print "\n"
